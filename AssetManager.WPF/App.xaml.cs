ca02dead9e4f1061dee54d14842c887ad061ed77
YaoRaoYaoRao <rencongyanggamestudio@gmail.com>
2024-03-31 22:10:28 +0800

    AssetManager.WPF:初始化Prism框架和MahApps框架

--- AssetManager.WPF/App.xaml.cs
+++ AssetManager.WPF/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using AssetManager.WPF.Views;
+using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace AssetManager.WPF
@@ -7,8 +8,17 @@ namespace AssetManager.WPF
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+           
+        }
     }
 
 }
--- AssetManager.WPF/Views/MainView.xaml.cs
+++ AssetManager.WPF/Views/MainView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +11,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AssetManager.WPF
+namespace AssetManager.WPF.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : MetroWindow
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
